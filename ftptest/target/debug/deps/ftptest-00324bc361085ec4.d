52f85a09ae436070f4f85ccd7c2159454900b1b7
fan@fan-VirtualBox.com <fan@fan-VirtualBox>
2022-06-28 17:29:52 +0800

    commit ftptest file

---
+++ ftptest/target/debug/deps/ftptest-00324bc361085ec4.d
@@ -0,0 +1,5 @@
+/home/fan/test/ftptest/target/debug/deps/ftptest-00324bc361085ec4.rmeta: src/main.rs
+
+/home/fan/test/ftptest/target/debug/deps/ftptest-00324bc361085ec4.d: src/main.rs
+
+src/main.rs:
---
+++ ftptest/target/debug/deps/ftptest-01ce2f5d4f55f6ed.d
@@ -0,0 +1,5 @@
+/home/fan/test/ftptest/target/debug/deps/ftptest-01ce2f5d4f55f6ed.rmeta: src/main.rs
+
+/home/fan/test/ftptest/target/debug/deps/ftptest-01ce2f5d4f55f6ed.d: src/main.rs
+
+src/main.rs:
